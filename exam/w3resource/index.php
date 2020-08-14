fa59c548f5971b865732020ede7e7692036bfcd7
screenleon <screen.leon@gmail.com>
2020-08-14 11:53:43 +0800

    Update the home page layout

--- exam/w3resource/index.php
+++ exam/w3resource/index.php
@@ -1,7 +1,12 @@
 <?php
 $fileList = glob('*.php');
-foreach ($fileList as $filename) : ?>
+foreach ($fileList as $key => $filename) : ?>
     <?php if ($filename !== 'index.php') : ?>
-        <a href="<?php echo $filename ?>"><?php echo $filename ?></a><br>
-    <?php endif; ?>
+        <a href="<?php echo $filename ?>"><?php echo $filename ?></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <?php endif; 
+        if(($key + 1) % 5 === 0){
+            echo "<br>";
+        }
+    ?>
+
 <?php endforeach; ?>
